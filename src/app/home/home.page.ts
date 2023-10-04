5f7a6d21f0781d44dd8b2f06ed4446d7a4d9e1a9
Rudi <rudihoffmann95@gmx.de>
2023-10-04 13:34:24 +0200

    html and scss defined for home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  score = 0;
 
-  constructor() {}
+  constructor(
+    private navCtrl: NavController,
+  ) {}
 
+
+  play() {
+    this.navCtrl.navigateForward("/play");
+  }
 }
